75374a8e35ed80bd33be6b09345afafa9ce4f074
MariaArz <marlin_13_1996@mail.ru>
2021-01-31 20:23:42 +0300

    Caesar with main letter

---
+++ XOstring/cmake-build-debug/CMakeFiles/XOstring.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XOstring.dir/main.cpp.obj"
+  "XOstring.exe"
+  "XOstring.exe.manifest"
+  "XOstring.pdb"
+  "libXOstring.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XOstring.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
