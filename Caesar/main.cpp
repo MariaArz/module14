0437e5f7e85c20de16228690a6e527049b4111a6
MariaArz <marlin_13_1996@mail.ru>
2021-02-02 18:37:07 +0300

    Caesar with main letter change

--- Caesar/main.cpp
+++ Caesar/main.cpp
@@ -6,7 +6,7 @@ std::string encryptCaesar(std::string string,int a){
             b=(int)string[i];
             b+=a;
             if ((string[i] >'a' && b > (int)'z')
-                || (string[i] <'a' && b > (int)'Z')){
+                || (string[i] >'A' && string[i] <'Z' && b > (int)'Z')){
                 b-=26;
             }
             string[i]=(char)b;
@@ -14,14 +14,16 @@ std::string encryptCaesar(std::string string,int a){
     }
     return string;
 }
-std::string decryptCaesar(std::string string,int a){
-    a=26-a;
-    string=encryptCaesar(string,a);
-    return string;
+std::string decryptCaesar(std::string str,int b){
+    b=26-b;
+    str=encryptCaesar(str,b);
+    return str;
 }
 int main() {
-    std::string string,over;
-    int a,b;
+    std::string string;
+    std::string over;
+    int a;
+    int n;
     std::cout<<"input massage\n";
     std::getline(std::cin,string);
     std::cout<<"\ninput number\n";
@@ -31,8 +33,8 @@ int main() {
     std::cout<<"\ninput massage\n";
     std::getline(std::cin,over);
     std::cout<<"\ninput number\n";
-    std::cin>>b;
-    over=decryptCaesar(over,b);
+    std::cin>>n;
+    over=decryptCaesar(over,n);
     std::cout<<over;
     return 0;
 }
